114f0eba6932c1143cd8d25aa5e627944bbd92aa
Anton Lindholm <LinAnt@users.noreply.github.com>
2024-02-16 09:35:08 +0200

    Migrate hacktop

---
+++ dot_config/tmux/theme.tmux
@@ -0,0 +1,27 @@
+thm_bg="#000000"
+thm_fg="#eaeaea"
+thm_cyan="#179299"
+thm_black="#e6e9ef"
+thm_gray="#bcc0cc"
+thm_magenta="#ea76cb"
+thm_pink="#8839ef"
+thm_red="#d20f39"
+thm_green="#40a02b"
+thm_yellow="#df8e1d"
+thm_blue="#1e66f5"
+thm_orange="#fe640b"
+thm_black4="#acb0be"
+
+# Color key:
+#   #000000 Background
+#   #eaeaea Foreground
+#   #2a2a2a Current Line
+#   #424242 Selection
+#   #969896 Comment
+#   #d54e53 Red
+#   #e78c45 Orange
+#   #e7c547 Yellow
+#   #b9ca4a Green
+#   #70c0b1 Aqua
+#   #7aa6da Blue
+#   #c397d8 Purple
---
+++ dot_oh-my-zsh/custom/plugins/ls/ls.plugin.zsh
@@ -0,0 +1,137 @@
+#!/usr/bin/env zsh
+
+# Standarized $0 handling, following:
+# https://z-shell.github.io/zsh-plugin-assessor/Zsh-Plugin-Standard
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+local _DIRNAME="${0:h}"
+
+# Remove any existing alias of ls
+if [[ $(alias ls) ]]; then
+  unalias ls
+fi
+
+if [[ -z "$ZSH_LS_BACKEND" ]]; then
+  if [[ ! -z "$ZSH_LS_PREFER_LS" ]]; then
+    ZSH_LS_BACKEND='ls'
+  elif (( $+commands[lsd] )); then
+    ZSH_LS_BACKEND='lsd'
+  elif (( $+commands[eza] )); then
+    ZSH_LS_BACKEND='eza'
+  elif (( $+commands[exa] )); then
+    ZSH_LS_BACKEND='exa'
+  else
+    ZSH_LS_BACKEND='ls'
+  fi
+fi
+
+if [[ "$ZSH_LS_BACKEND" == "lsd" ]]; then
+  typeset -g lsd_params; lsd_params=()
+
+  function ls() {
+    lsd ${lsd_params} $@
+  }
+  compdef ls=lsd
+
+  function l() {
+    lsd ${lsd_params} $@
+  }
+  compdef l=lsd
+
+  function la() {
+    lsd -a ${lsd_params} $@
+  }
+  compdef la=lsd
+
+  function ll() {
+    lsd --header --long ${lsd_params} $@
+  }
+  compdef ll=lsd
+
+  function lt() {
+    lsd --tree ${lsd_params} $@
+  }
+  compdef lt=lsd
+elif [[ "$ZSH_LS_BACKEND" == "exa" || "$ZSH_LS_BACKEND" == "eza" ]]; then
+  typeset -g exa_params; exa_params=('--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color=auto')
+
+  if ((! ${+ZSH_LS_DISABLE_GIT})); then
+    exa_params+=('--git')
+  fi
+
+  function ls() {
+    $ZSH_LS_BACKEND ${exa_params} $@
+  }
+  compdef ls=$ZSH_LS_BACKEND
+
+  function l() {
+    $ZSH_LS_BACKEND --git-ignore ${exa_params} $@
+  }
+  compdef l=$ZSH_LS_BACKEND
+
+  function la() {
+    $ZSH_LS_BACKEND -a ${exa_params} $@
+  }
+  compdef la=$ZSH_LS_BACKEND
+
+  function ll() {
+    $ZSH_LS_BACKEND --header --long ${exa_params} $@
+  }
+  compdef ll=$ZSH_LS_BACKEND
+
+  function lt() {
+    $ZSH_LS_BACKEND --tree ${exa_params} $@
+  }
+  compdef lt=$ZSH_LS_BACKEND
+else
+  typeset -g _ls
+  _ls=(=ls)
+
+  if (( $+commands[gls] )); then
+    _ls=(=gls)
+  fi
+
+  typeset -g _ls_params
+  _ls_params=('-hF' '--group-directories-first' '--time-style=+%Y-%m-%d %H:%M' '--quoting-style=literal')
+
+  if ${_ls[@]} --hyperlink >/dev/null 2>&1 ; then
+    _ls_params+=('--hyperlink')
+  fi
+
+  if [[ "$CLICOLOR" != "0" ]]; then
+    _ls_params+=('--color')
+  fi
+
+  if (( $+commands[grc] )); then
+    _grc=('grc' "--config=${_DIRNAME}/conf.ls" )
+  fi
+
+  function ls() {
+    $_ls ${_ls_params} -C $@
+  }
+  compdef ls=ls
+
+  function l() {
+    $_ls ${_ls_params} -C $@
+  }
+  compdef l=ls
+
+  function la() {
+    $_ls ${_ls_params} -C -A $@
+  }
+  compdef la=ls
+
+  function ll() {
+    if [[ "$CLICOLOR" != "0" ]]; then
+      $_grc $_ls ${_ls_params} -l $@
+    else
+      $_ls -l $@
+    fi
+  }
+  compdef ll=ls
+
+  function lt() {
+    find . -print | sed -e 's;[^/]*/;|;g;s;|; |;g'
+  }
+fi
+
